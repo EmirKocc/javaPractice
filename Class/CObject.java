0492a21088b6fcb5f4b797379a5a6564d1290366
Emir Koç <116124842+EmirKocc@users.noreply.github.com>
2024-02-27 19:23:57 +0300

    Add files via upload

---
+++ Class/CObject.java
@@ -0,0 +1,24 @@
+package Class;
+
+public class CObject {
+
+	public static void main(String[] args) {
+	int asalCount = 0 ;
+	int number =25 ;
+	
+	for (int i = 1; i ==number; i++) {
+		if (number % i == 0) {
+			asalCount++;
+			}
+	if (asalCount == 2 ) {
+		
+		System.out.println("sayı asladır");
+	}
+	else {
+		System.out.println("sayı asal değildir");
+	}
+
+}
+
+	}
+		}
